ee62be472830249ac5cf23bf58163b9a895f2339
ignacio-231 <icontardo2024@inf.udec.cl>
2025-10-30 20:17:19 -0300

    carpetas

--- programa/lib/main.dart
+++ programa/lib/main.dart
@@ -1,18 +1,76 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const MenuPrincipal(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MenuPrincipal extends StatelessWidget {
+  const MenuPrincipal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(221, 33, 16, 105),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 205, 255, 7),
+        title: const Text('Pokédex App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              icon: const Icon(Icons.catching_pokemon),
+              label: const Text('Pokédex'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(248, 73, 143, 32),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 16,
+                ),
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ListaPokemon()),
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              icon: const Icon(Icons.group),
+              label: const Text('Mi Equipo'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(248, 73, 143, 32),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 16,
+                ),
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ()),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
